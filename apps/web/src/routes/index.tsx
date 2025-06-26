a8920e28c6d2bc1cd445120b7bf6f6890c066e1f
paulmakemehappy <148147999+paulmakemehappy@users.noreply.github.com>
2025-06-27 00:00:50 +0200

    add shared tailwind and remove component generate

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1 className="text-2xl font-bold mb-4">PostHog Test</h1>
       <button
         onClick={onclick}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-red-500"
+        className=" hover:bg-blue-700 bg-brand text-white font-bold py-2 px-4 rounded disabled:bg-red-500"
         disabled={!posthog.__loaded}
       >
         Fire Posthog Event
