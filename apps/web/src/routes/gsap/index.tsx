524b3f46c607f6fc2cdf927f5f4c035ed03b4fa8
paulmakemehappy <paul.schrbr@icloud.com>
2025-06-28 00:04:11 +0200

    small text changes

--- apps/web/src/routes/gsap/index.tsx
+++ apps/web/src/routes/gsap/index.tsx
@@ -7,14 +7,14 @@ export const Route = createFileRoute("/gsap/")({
 
 function RouteComponent() {
   return (
-    <div className=" flex flex-col items-center justify-center gap-6 p-8">
+    <div className=" flex flex-col items-center justify-center gap-6 p-12 mt-20">
       <button
         onClick={() =>
-          dynamicIsland("Dynamic Island notification", { duration: 4000 })
+          dynamicIsland("Simulated Toast Notification", { duration: 4000 })
         }
         className="bg-gray-800 hover:bg-gray-900 text-white font-medium px-6 py-3 rounded-lg shadow-lg transition-all duration-200 hover:scale-105 active:scale-95"
       >
-        Trigger Dynamic Island
+        Simulate Toast
       </button>
     </div>
   );
