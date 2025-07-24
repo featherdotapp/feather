eb30d3a4a1ae58ab8c80be4b35596ee7d3314065
adrianbatista <adrian.b11@schule.bremen.de>
2025-07-24 21:40:16 +0200

    Complete test coverage

--- apps/api/src/test/java/com/feather/api/security/provider/JwtTokenAuthenticationProviderTest.java
+++ apps/api/src/test/java/com/feather/api/security/provider/JwtTokenAuthenticationProviderTest.java
@@ -1,19 +1,5 @@
 package com.feather.api.security.provider;
 
-import static com.feather.api.shared.TokenType.ACCESS_TOKEN;
-import static com.feather.api.shared.TokenType.REFRESH_TOKEN;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-
 import com.feather.api.jpa.model.User;
 import com.feather.api.jpa.service.JwtTokenService;
 import com.feather.api.jpa.service.UserService;
@@ -34,6 +20,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.feather.api.shared.TokenType.ACCESS_TOKEN;
+import static com.feather.api.shared.TokenType.REFRESH_TOKEN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class JwtTokenAuthenticationProviderTest {
@@ -71,7 +67,6 @@ class JwtTokenAuthenticationProviderTest {
         // Arrange
         final String accessToken = "valid-access-token";
         final String refreshToken = "valid-refresh-token";
-        final String currentCreds = "current-creds";
         final SimpleGrantedAuthority apiKeyAuthority = new SimpleGrantedAuthority(AuthenticationRoles.WITH_API_KEY.name());
         final SimpleGrantedAuthority jwtTokenAuthority = new SimpleGrantedAuthority(AuthenticationRoles.WITH_JWT_TOKEN.name());
         final List<GrantedAuthority> value = new ArrayList<>(Collections.singleton(apiKeyAuthority));
@@ -84,7 +79,6 @@ class JwtTokenAuthenticationProviderTest {
         when(jwtTokenService.isJwtTokenValid(accessToken, user, ACCESS_TOKEN)).thenReturn(true);
 
         securityContextHolderMock.when(() -> SecurityContextHolder.getContext().getAuthentication()).thenReturn(currentAuthentication);
-        when(currentAuthentication.getCredentials()).thenReturn(currentCreds);
         doReturn(value).when(currentAuthentication).getAuthorities();
 
         // Act
@@ -100,46 +94,58 @@ class JwtTokenAuthenticationProviderTest {
     }
 
     @Test
-    void testAuthenticate_expiredAccessToken_validRefreshToken() {
+    void testAuthenticate_expiredAccessToken_throwsJwtAuthenticationException() {
         // Arrange
         final String expiredAccessToken = "expired-access-token";
-        final String validRefreshToken = "valid-refresh-token";
-        final String currentCreds = "current-creds";
-        final String newAccessToken = "new-access-token";
+        final String refreshToken = "refresh-token";
 
         when(authentication.getCredentials()).thenReturn(credentials);
         when(authentication.getPrincipal()).thenReturn(user);
         when(credentials.accessToken()).thenReturn(expiredAccessToken);
-        when(credentials.refreshToken()).thenReturn(validRefreshToken);
+        when(credentials.refreshToken()).thenReturn(refreshToken);
+        when(jwtTokenService.isJwtTokenValid(expiredAccessToken, user, ACCESS_TOKEN)).thenReturn(true);
         when(jwtTokenService.isTokenExpired(expiredAccessToken)).thenReturn(true);
-        when(jwtTokenService.isTokenExpired(validRefreshToken)).thenReturn(false);
-        when(jwtTokenService.isJwtTokenValid(validRefreshToken, user, REFRESH_TOKEN)).thenReturn(true);
-        when(jwtTokenService.generateJwtToken(user, ACCESS_TOKEN)).thenReturn(newAccessToken);
 
-        securityContextHolderMock.when(() -> SecurityContextHolder.getContext().getAuthentication()).thenReturn(currentAuthentication);
-        when(currentAuthentication.getCredentials()).thenReturn(currentCreds);
-        when(currentAuthentication.getAuthorities()).thenReturn(Set.of());
+        // Act & Assert
+        final JwtAuthenticationException exception = assertThrows(
+                JwtAuthenticationException.class,
+                () -> classUnderTest.authenticate(authentication)
+        );
+        assertThat(exception.getMessage()).isEqualTo("Expired Access Token");
+    }
 
-        // Act
-        final Authentication result = classUnderTest.authenticate(authentication);
+    @Test
+    void testAuthenticate_invalidAccessToken_throwsJwtAuthenticationException() {
+        // Arrange
+        final String invalidAccessToken = "invalid-access-token";
+        final String refreshToken = "refresh-token";
 
-        // Assert
-        assertThat(result).isInstanceOf(FeatherAuthenticationToken.class);
-        verify(userService).updateUserToken(user, newAccessToken, REFRESH_TOKEN);
-        verify(jwtTokenService).generateJwtToken(user, ACCESS_TOKEN);
+        when(authentication.getCredentials()).thenReturn(credentials);
+        when(authentication.getPrincipal()).thenReturn(user);
+        when(credentials.accessToken()).thenReturn(invalidAccessToken);
+        when(credentials.refreshToken()).thenReturn(refreshToken);
+        when(jwtTokenService.isTokenExpired(refreshToken)).thenReturn(false);
+        when(jwtTokenService.isJwtTokenValid(invalidAccessToken, user, ACCESS_TOKEN)).thenReturn(false);
+
+        // Act & Assert
+        final JwtAuthenticationException exception = assertThrows(
+                JwtAuthenticationException.class,
+                () -> classUnderTest.authenticate(authentication)
+        );
+        assertThat(exception.getMessage()).isEqualTo("Invalid Refresh Token");
     }
 
     @Test
     void testAuthenticate_expiredRefreshToken_throwsJwtAuthenticationException() {
         // Arrange
-        final String expiredAccessToken = "expired-access-token";
+        final String accessToken = "access-token";
         final String expiredRefreshToken = "expired-refresh-token";
 
         when(authentication.getCredentials()).thenReturn(credentials);
         when(authentication.getPrincipal()).thenReturn(user);
-        when(credentials.accessToken()).thenReturn(expiredAccessToken);
+        when(credentials.accessToken()).thenReturn(accessToken);
         when(credentials.refreshToken()).thenReturn(expiredRefreshToken);
-        when(jwtTokenService.isTokenExpired(expiredAccessToken)).thenReturn(true);
+        when(jwtTokenService.isJwtTokenValid(accessToken, user, ACCESS_TOKEN)).thenReturn(false);
         when(jwtTokenService.isTokenExpired(expiredRefreshToken)).thenReturn(true);
 
         // Act & Assert
@@ -153,14 +159,14 @@ class JwtTokenAuthenticationProviderTest {
     @Test
     void testAuthenticate_invalidRefreshToken_throwsJwtAuthenticationException() {
         // Arrange
-        final String expiredAccessToken = "expired-access-token";
+        final String accessToken = "access-token";
         final String invalidRefreshToken = "invalid-refresh-token";
 
         when(authentication.getCredentials()).thenReturn(credentials);
         when(authentication.getPrincipal()).thenReturn(user);
-        when(credentials.accessToken()).thenReturn(expiredAccessToken);
+        when(credentials.accessToken()).thenReturn(accessToken);
         when(credentials.refreshToken()).thenReturn(invalidRefreshToken);
-        when(jwtTokenService.isTokenExpired(expiredAccessToken)).thenReturn(true);
+        when(jwtTokenService.isJwtTokenValid(accessToken, user, ACCESS_TOKEN)).thenReturn(false);
         when(jwtTokenService.isTokenExpired(invalidRefreshToken)).thenReturn(false);
         when(jwtTokenService.isJwtTokenValid(invalidRefreshToken, user, REFRESH_TOKEN)).thenReturn(false);
 
@@ -173,25 +179,37 @@ class JwtTokenAuthenticationProviderTest {
     }
 
     @Test
-    void testAuthenticate_invalidAccessToken_throwsJwtAuthenticationException() {
+    void testAuthenticate_invalidAccessToken_validRefreshToken() {
         // Arrange
         final String invalidAccessToken = "invalid-access-token";
-        final String refreshToken = "refresh-token";
+        final String validRefreshToken = "valid-refresh-token";
+        final SimpleGrantedAuthority apiKeyAuthority = new SimpleGrantedAuthority(AuthenticationRoles.WITH_API_KEY.name());
+        final SimpleGrantedAuthority jwtTokenAuthority = new SimpleGrantedAuthority(AuthenticationRoles.WITH_JWT_TOKEN.name());
+        final List<GrantedAuthority> value = new ArrayList<>(Collections.singleton(apiKeyAuthority));
 
         when(authentication.getCredentials()).thenReturn(credentials);
         when(authentication.getPrincipal()).thenReturn(user);
         when(credentials.accessToken()).thenReturn(invalidAccessToken);
-        when(credentials.refreshToken()).thenReturn(refreshToken);
-        when(jwtTokenService.isTokenExpired(invalidAccessToken)).thenReturn(false);
+        when(credentials.refreshToken()).thenReturn(validRefreshToken);
         when(jwtTokenService.isJwtTokenValid(invalidAccessToken, user, ACCESS_TOKEN)).thenReturn(false);
+        when(jwtTokenService.isTokenExpired(validRefreshToken)).thenReturn(false);
+        when(jwtTokenService.isJwtTokenValid(validRefreshToken, user, REFRESH_TOKEN)).thenReturn(true);
 
-        // Act & Assert
-        final JwtAuthenticationException exception = assertThrows(
-                JwtAuthenticationException.class,
-                () -> classUnderTest.authenticate(authentication)
-        );
-        assertThat(exception.getMessage()).isEqualTo("Invalid Access Token");
+        securityContextHolderMock.when(() -> SecurityContextHolder.getContext().getAuthentication()).thenReturn(currentAuthentication);
+        doReturn(value).when(currentAuthentication).getAuthorities();
+
+        // Act
+        final Authentication result = classUnderTest.authenticate(authentication);
+
+        // Assert
+        assertThat(result).isInstanceOf(FeatherAuthenticationToken.class);
+        final FeatherAuthenticationToken resultToken = (FeatherAuthenticationToken) result;
+        assertThat(resultToken.getPrincipal()).isEqualTo(user);
+        assertThat(resultToken.getAuthorities()).hasSize(2);
+        assertThat(resultToken.getAuthorities()).contains(apiKeyAuthority);
+        assertThat(resultToken.getAuthorities()).contains(jwtTokenAuthority);
     }
+
 
     @Test
     void testSupports_returnsTrueForFeatherAuthenticationToken() {
