2adccfc576ec98a572795fd7c561133506b76e16
adrianbatista <adrian.batistaberinger@gmail.com>
2025-08-01 11:28:29 +0200

    sonar

--- apps/web/src/sonarTest.ts
+++
@@ -1,8 +0,0 @@
-// Simple TypeScript code to trigger a frontend change for Sonar
-export function add(a: number, b: number): number {
-    return a + b;
-}
-
-// Example usage
-console.log('Sum:', add(2, 2));
-
